aca09e1d09db0c3786e108c616752ef5b7af36d2
Veerpaliwal <veerpaliwal901gmail.com>
2025-07-19 02:16:27 -0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 // import area
 // import {namedImport} form somelib/someloction
-
+// import { useState } from 'react';
 
 // import someloction
 import './App.css'
 
 // global variable
-
+// let fname = 'veer'; // this is way of create javascript global variable
 
 
 // function definaction area
 function App() {
 // local variable
-   
+// let lname = 'paliwal'; // this way of create javascript local variable 
 
 //1. define hook variable
 // const [variable,setfunction] = useState(initialvalue);
@@ -22,7 +22,7 @@ function App() {
 
 
 //3. return statement
-  
+// const [age,setAge] = useState(20);
 
   return (
     <>
