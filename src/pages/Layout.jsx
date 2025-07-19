1e7bee67dd2bd3660478b53f873a6ec3596cec8a
Veerpaliwal <veerpaliwal901gmail.com>
2025-07-19 01:55:03 -0700

    new

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 export default function Layout() {
   return (
     <>
-      <header>
+      <header >
          header
       </header>
       <main className="row">
